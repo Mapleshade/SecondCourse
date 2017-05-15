c6d8d62418e1a10c8738b2e6b68a698bad2add8a
Elina Gazizova <lady.gazizova2011@yandex.ru>
2017-05-15 04:49:10 +0300

    Дампы

---
+++ Semestrovka_KotikiShop/test_products.sql
@@ -0,0 +1,3 @@
+INSERT INTO test.products (description, img, name, price, type) VALUES ('happy kitten', 'https://img1.etsystatic.com/109/0/10642867/il_170x135.868406497_n62a.jpg', 'Bed', 1500, 'Beds'); 
+INSERT INTO test.products (description, img, name, price, type) VALUES ('nice choice', 'http://cdn.xl.thumbs.canstockphoto.com/canstock14634128.jpg', 'Ball', 500, 'Toys'); 
+INSERT INTO test.products (description, img, name, price, type) VALUES ('good toy', 'https://s-media-cache-ak0.pinimg.com/236x/5c/1a/1a/5c1a1a90647ad317999a4d85ba1ce5ca.jpg', 'Mouse', 300, 'Toys'); 
---
+++ Semestrovka_KotikiShop/test_stocks.sql
@@ -0,0 +1,3 @@
+INSERT INTO test.stocks (city, street) VALUES ('Moskva', 'Arbat 3');
+INSERT INTO test.stocks (sity, street) VALUES ('Kazan', 'Kremlevskaya 2');
+INSERT INTO test.stocks (sity, street) VALUES ('Myrlyandiya', 'Myrchanskogo 1');
